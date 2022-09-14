ab97827b3673f2a30da14cc2d27a94395254f18c
Jimykiz <raphaelemerald2002@gmail.com>
2022-09-14 06:53:27 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,7 @@
  *
  * Return: will return (0) if success
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,7 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('/n');
+	_putchar('\n');
 
 	return (0);
 }
