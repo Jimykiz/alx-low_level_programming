a8a41e3bef76c921fc360a14a1c753f7aafd833b
Jimykiz <raphaelemerald2002@gmail.com>
2022-09-14 07:44:11 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+
+/**
+ * print_sign - compute the absolute value.
+ *
+ * @n: to be checked
+ *
+ * * Return: Always 0.
+ */
+
+int print_sign(int n)
+
+{
+
+		if (n > 0)
+		{
+		_putchar('+');
+		return (1);
+		}
+		else if (n < 0)
+		{
+			_putchar('_');
+			return (-1);
+		}
+		else
+{
+			_putchar('0');
+			return (0);
+
+}
